441689bdf4c7676744103f7bbc159a14b3d1e1ea
shesh913111 <76205307+shesh913111@users.noreply.github.com>
2023-10-18 22:12:04 +0530

    TestNg
    TestngFramework

---
+++ TestNG/src/main/java/Ayansys_solutions/TestNG/App.java
@@ -0,0 +1,13 @@
+package Ayansys_solutions.TestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestNG/src/test/java/Ayansys_solutions/TestNG/TestNg1.java
@@ -0,0 +1,18 @@
+package Ayansys_solutions.TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNg1 {
+
+	@Test
+	public int add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		return c;
+		
+	}
+	
+  
+}
